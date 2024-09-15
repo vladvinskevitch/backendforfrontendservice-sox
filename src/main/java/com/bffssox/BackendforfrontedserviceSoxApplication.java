2f67e6e2b1f21ba428b8c719e31d1b7da1932714
m83city <rostorguievv@gmail.com>
2024-09-15 18:52:11 +0300

    create base project from initilizr

---
+++ src/main/java/com/bffssox/BackendforfrontedserviceSoxApplication.java
@@ -0,0 +1,13 @@
+package com.bffssox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendforfrontedserviceSoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendforfrontedserviceSoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bffssox/BackendforfrontedserviceSoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bffssox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendforfrontedserviceSoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
