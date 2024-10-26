746e49ad5c67e1b48ad4c5837fbbad499f172064
m83city <rostorguievv@gmail.com>
2024-10-26 17:45:48 +0300

    feat: fix bug with AuthClient be null

--- application/src/main/java/com/sitionix/bffssox/usecase/CreateUserImpl.java
+++ application/src/main/java/com/sitionix/bffssox/usecase/CreateUserImpl.java
@@ -2,12 +2,14 @@ package com.sitionix.bffssox.usecase;
 
 import com.sitionix.bffssox.client.AuthClient;
 import com.sitionix.bffssox.domain.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreateUserImpl implements CreateUser {
 
-    private AuthClient authClient;
+    private final AuthClient authClient;
 
     @Override
     public User execute(User user) {
